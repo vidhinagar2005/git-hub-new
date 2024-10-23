79b83cd3fe5158f6b4d92a1c82fcf1cf13539226
vidhinagar2005 <viddhinagar@gmail.com>
2024-10-23 21:03:48 +0530

    light

---
+++ oops/a/light.py
@@ -0,0 +1,26 @@
+class traficlight:
+    def __init__(self, color):
+        self.color = color
+
+    def action(self):
+        if self.color == "red":
+            self.next_color = "yellow"
+            print("stop and wait")
+
+        elif self.color == "yellow":
+            self.next_color = "green"
+            print("prepare and stop")
+
+        elif self.color == "green":
+            self.next_color = "red"
+            print("go")
+        else:
+            self.next_color = "orange"
+            print("stop")
+
+for i in ["red","yello","green"]:
+    t1 = traficlight(i)
+    print(t1.color)
+    t1.action()
+    print(t1.next_color)
+    print("\n")
