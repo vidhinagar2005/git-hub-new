e90fd0151b9c72f42ac9ad12e720afe0936bab9d
vidhinagar2005 <viddhinagar@gmail.com>
2024-10-23 22:31:02 +0530

    cloning

---
+++ oops/a/unit2_15.py
@@ -0,0 +1,13 @@
+list1 = [i for i in range(1,6)]
+list2 = [i for i in range(7,11)]
+print(list1)
+print(list2)
+
+list3 = list1 + list2
+print(list3)
+
+new_list = [i for i in list3 for x in (0, 1)]
+print(new_list)
+
+list_cloning = list3[:]
+print(list_cloning)
