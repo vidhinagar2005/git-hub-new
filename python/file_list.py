1df582441ec2aadb5c3a2e35626de7d61e808fae
vidhinagar2005 <viddhinagar@gmail.com>
2024-10-25 21:39:43 +0530

    prime number

--- python/file_list.py
+++ python/file_list.py
@@ -1,5 +1,16 @@
 list = [1,2,11,28,29,50]
 f1 = open("number.txt","w")
+f2 = open("prime.txt","w")
 for i in list:
-    f1.write(str(i))
+    
     f1.write("\n")
+    if i % 2 == 0:
+        # f1.write(str(i))
+        # f2.write(str(i))
+        f1.write("Not prime number...")
+        f1.write("\n")
+
+    else:
+        f2.write(str(i))
+        f2.write("prime number...")
+        f2.write("\n")
