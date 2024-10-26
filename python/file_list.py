e9f5147a366f38930302260de2249d34e552b7b4
vidhinagar2005 <viddhinagar@gmail.com>
2024-10-26 21:15:55 +0530

    running

--- python/file_list.py
+++ python/file_list.py
@@ -1,19 +1,20 @@
 list = [1,2,3,4,5,6,7,8,9,10]
-f1 = open("data.txt","w")
-for i in list:
-    f1.write(str(i))
-    f1.write("\n")
+# f1 = open("data.txt","w")
+# for i in list:
+#     f1.write(str(i))
+#     f1.write("\n")
 
-f1.close()
+# f1.close()
 
 f2 = open("data.txt","r")
+msg = f2.readlines()
+f3 = open("num.txt","a")
+main = f3.write(str(msg))
 
-for k in f2:
-    f2.readlines()
-    f3 = open("num.txt","a")
-    
-    f3.write(k)
-
+for k in main:
+    if k % 2 == 0:
+        
+        f3.write(str("divide"))
 
     
 
