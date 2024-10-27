e6bed51d052bef99f8496aab1f223ef82e5bc625
vidhinagar2005 <viddhinagar@gmail.com>
2024-10-27 18:34:12 +0530

    armstrong

--- python/task2.py
+++ python/task2.py
@@ -1,14 +1,19 @@
 a = 0
 b = 1
 ans = 0
-print(a,b)
-choice = int(input("entewr the number = "))
-for i in range(2,choice):
-    #   print(i)
-      ans = i + ans
-      print(ans)
+print("a = ",a,"b = ",b)
+num = int(input("Enter the number = "))
+new = num
+while num > 0:
+    r = num%10
+    
+    ans += r ** 3
+    print(ans)
+    num = num //10
 
+if ans == new:
+    print("armstrong")
 
-
-
+else:
+    print("not armstrong")
 
