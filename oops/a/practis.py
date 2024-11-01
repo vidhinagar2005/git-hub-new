512230ccf1c1a7c8d78eed66b67c1ae9c7b73383
vidhinagar2005 <viddhinagar@gmail.com>
2024-11-01 17:26:11 +0530

    inheritan

--- oops/a/practis.py
+++ oops/a/practis.py
@@ -1,11 +1,20 @@
-class student:
-    def setid(self,id):
-        self.id = id
-    def getid(self):
-        return self.id
-    def setname(self, name):
-        self.name = name
-    def getname(self):
-        return self.name
+class square:
+    def __init__(self,x):
+        self.x = x
 
-    
+    def area(self):
+        print("Area of square = ",self.x * self.x)
+
+class rectangle(square):
+    def __init__(self,a,b):
+        super().__init__(a)
+        self.b = b
+
+    def area(self):
+        super().area()
+        print("Area of rectangle = ",self.x * self.b)
+
+v = int(input("length = "))
+i = int(input("Breath = "))
+r = rectangle(v,i)
+r.area()
