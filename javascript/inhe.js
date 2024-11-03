366b8f0b7777306f36f009ed1a765fffef9c093a
vidhinagar2005 <viddhinagar@gmail.com>
2024-11-03 17:34:08 +0530

    inheritance

--- javascript/inhe.js
+++ javascript/inhe.js
@@ -1,1 +1,25 @@
-class 
+class parent{
+
+   
+
+    age(num){
+        this.num = num;
+        console.log(this.num);
+    }
+
+    name(fullname){
+        this.fullname = fullname;
+        console.log(this.fullname);
+    }
+    
+
+    
+}
+
+
+c1 = new parent();
+console.log(c1);
+c1.age(23);
+c1.name("vidhi");
+
+
