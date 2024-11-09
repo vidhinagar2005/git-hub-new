0a27feaf6a9f7aee4cc70b9814cf2eaa879b82c9
vidhinagar2005 <viddhinagar@gmail.com>
2024-11-09 16:12:02 +0530

    lamda

--- javascript/promise.js
+++ javascript/promise.js
@@ -1,6 +1,11 @@
-let promise = new promise(() =>{
-    console.log("I am promise");
-    
-    
-
-});
+function getdata(id,nextdata){
+    return promise = new Promise((resolve,reject) => {
+        setTimeout(() =>{
+            console.log("data = ",id);
+            resolve("sucess");
+            if(nextdata){
+                nextdata();
+            }
+        },5000);
+    });
+}
---
+++ python/lamda.py
@@ -0,0 +1,8 @@
+add = lambda x,y: x + y
+result = add(5,3)
+print(result)
+
+
+sub = lambda x,y: x - y
+result = sub(10,5)
+print(result)
