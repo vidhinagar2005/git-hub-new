088d0d84581f85c93b7db349202584bd0afe0338
vidhinagar2005 <viddhinagar@gmail.com>
2024-12-11 23:08:45 +0530

    menu

---
+++ oops/a/unit1_9.py
@@ -0,0 +1,25 @@
+choice = 0
+while choice != 3:
+    print("1.circle")
+    print("2.triangle")
+    print("3. exit")
+    choice = int(input("enter choice = "))
+    if choice == 1:
+        pi = 3.14
+        radius = float(input("enter radius = "))
+        area = pi * radius * radius
+        print(area)
+
+    elif choice == 2:
+        a = float(input("first side = "))
+        b = float(input("second side = "))
+        c = float(input("third side = "))
+        s = (a + b + c)/2
+        area = (s * (s-a)*(s-b)*(s-c)) ** 0.5
+        print(area)
+    elif choice == 3:
+        exit()
+
+    else:
+        print("bye bye")
+
